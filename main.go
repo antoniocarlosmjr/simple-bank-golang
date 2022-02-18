51a76e65f0f45b6299582d9316efa1349a96e645
Antonio Martins <antonio.pereira@picpay.com>
2022-02-18 15:57:09 -0300

    feat: add func withdraw

--- main.go
+++ main.go
@@ -18,5 +18,22 @@ func main() {
 	accountAntonio.owner = "Antonio"
 	accountAntonio.balance = 100.50
 
-	fmt.Println(*accountAntonio)
+	var accountLuana *Account
+	accountLuana = new(Account)
+	accountLuana.owner = "Luana"
+	accountLuana.balance = 50.
+
+	fmt.Println(accountAntonio.balance)
+	*accountAntonio = withdrawAccount(*accountAntonio, 100)
+	fmt.Println(accountAntonio.balance)
+}
+
+func withdrawAccount(account Account, value float64) Account {
+	if value <= account.balance {
+		account.balance = account.balance - value
+	} else {
+		fmt.Println("Impossível sacar valor, pois não possui saldo suficiente")
+	}
+
+	return account
 }
