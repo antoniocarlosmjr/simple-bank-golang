b50145f0f49590c269cf485f0510c3fee9ffbbb8
Antonio Martins <antonio.pereira@picpay.com>
2022-02-22 00:31:42 -0300

    feat: add func transfer and new comments in readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # simple-bank-golang
-This project was created for to use the basics concepts of Golang in a simple project of the commom banking.
+This project was created for to use the basics concepts of Golang in a simple project of the commom banking that do transfer, deposit and withdraw money in accounts.
+
+### Next steps
+* to add rules in services;
+* to use some design patterns;
+* transform all in API Rest;
+    * create routes;
+    * integrate with any framework (gin, etc)
--- main.go
+++ main.go
@@ -27,6 +27,9 @@ func main() {
 	fmt.Println(accountAntonio.balance)
 	fmt.Println(accountAntonio.deposit(100))
 	fmt.Println(accountAntonio.balance)
+
+	fmt.Println(accountAntonio.transfer(50, accountLuana))
+	fmt.Println(*accountAntonio, *accountLuana)
 }
 
 func (account *Account) withdraw(value float64) string {
@@ -36,14 +39,29 @@ func (account *Account) withdraw(value float64) string {
 		return "Saque realizado com sucesso"
 	}
 
-	return "Saldo insuficiente ou valor informado é inválido"
+	return "Saldo insuficiente ou valor informado para depósito é inválido"
 }
 
-func (account *Account) deposit(value float64) string {
+func (account *Account) deposit(value float64) (string, float64) {
 	if value > 0 {
 		account.balance += value
-		return "Depósito realizado com sucesso"
+		return "Depósito realizado com sucesso", account.balance
 	}
 
-	return "Valor do depósito é informado é inválido"
+	return "Valor do depósito informado é inválido", account.balance
+}
+
+func (accountOrigin *Account) transfer(value float64, accountTarget *Account) string {
+
+	balanceAccountOrigin := accountOrigin.balance
+
+	if value > 0 && value <= balanceAccountOrigin {
+		accountOrigin.balance -= value
+		accountTarget.deposit(value)
+
+		return "Transferência realizada com sucesso"
+	}
+
+	return "Valor é menor do que zero e conta de origem precisa de saldo em conta"
+
 }
