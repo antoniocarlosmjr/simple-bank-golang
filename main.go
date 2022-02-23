2599c7eaa9ab1f77e4117113846eec7fdb1a8ec1
Antonio Martins <antonio.pereira@picpay.com>
2022-02-22 23:41:13 -0300

    feat: add new visilibity for balance

--- main.go
+++ main.go
@@ -3,25 +3,31 @@ package main
 import (
 	"fmt"
 
+	"simple-bank-golang/entities"
 	"simple-bank-golang/services"
 )
 
 func main() {
+	userJoao := entities.User{"Joao", "1234", "Developer"}
+	accountJoao := entities.Account{userJoao, "001", "100", 0}
+
 	var accountAntonio *services.Account
 	accountAntonio = new(services.Account)
 	accountAntonio.User.Name = "Antonio"
-	accountAntonio.Balance = 100.50
+	accountAntonio.Deposit(100.50)
 
 	var accountLuana *services.Account
 	accountLuana = new(services.Account)
 	accountLuana.User.Name = "Luana"
-	accountLuana.Balance = 50.
+	accountAntonio.Deposit(50)
 
-	fmt.Println(accountAntonio.Balance)
+	fmt.Println(accountJoao)
+
+	fmt.Println(accountAntonio.SeeBalance())
 	fmt.Println(accountAntonio.Withdraw(10))
-	fmt.Println(accountAntonio.Balance)
+	fmt.Println(accountAntonio.SeeBalance)
 	fmt.Println(accountAntonio.Deposit(100))
-	fmt.Println(accountAntonio.Balance)
+	fmt.Println(accountAntonio.SeeBalance)
 
 	fmt.Println(accountAntonio.Transfer(50, accountLuana))
 	fmt.Println(*accountAntonio, *accountLuana)
--- services/account.go
+++ services/account.go
@@ -6,13 +6,13 @@ type Account struct {
 	User    entities.User
 	Agency  int
 	Account int
-	Balance float64
+	balance float64
 }
 
 func (account *Account) Withdraw(value float64) string {
-	hasBalance := value <= account.Balance
+	hasBalance := value <= account.balance
 	if hasBalance && value > 0 {
-		account.Balance -= value
+		account.balance -= value
 		return "Saque realizado com sucesso"
 	}
 
@@ -21,23 +21,27 @@ func (account *Account) Withdraw(value float64) string {
 
 func (account *Account) Deposit(value float64) (string, float64) {
 	if value > 0 {
-		account.Balance += value
-		return "Depósito realizado com sucesso", account.Balance
+		account.balance += value
+		return "Depósito realizado com sucesso", account.balance
 	}
 
-	return "Valor do depósito informado é inválido", account.Balance
+	return "Valor do depósito informado é inválido", account.balance
 }
 
 func (accountOrigin *Account) Transfer(value float64, accountTarget *Account) string {
 
-	balanceAccountOrigin := accountOrigin.Balance
+	balanceAccountOrigin := accountOrigin.balance
 
 	if value > 0 && value <= balanceAccountOrigin {
-		accountOrigin.Balance -= value
+		accountOrigin.balance -= value
 		accountTarget.Deposit(value)
 
 		return "Transferência realizada com sucesso"
 	}
 
 	return "Valor é menor do que zero e conta de origem precisa de saldo em conta"
+}
+
+func (account *Account) SeeBalance() float64 {
+	return account.balance
 }
